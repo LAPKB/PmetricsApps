283003dc8965e28a2cd5b32133fb2a6b01a6317c
Michael Neely <mneely@usc.edu>
2024-04-24 11:09:18 -0700

    CRAN checks

---
+++ R/PmetricsApps-package.R
@@ -0,0 +1,31 @@
+#' @keywords internal
+"_PACKAGE"
+
+## usethis namespace: start
+#' @import rlang
+#' @importFrom lifecycle deprecated
+## usethis namespace: end
+##
+#' @import Pmetrics
+#' @importFrom dplyr select arrange filter mutate transmute group_by row_number
+#' group_map ungroup bind_cols bind_rows nest_by rowwise rename inner_join
+#' slice_tail slice_head slice across tibble as_tibble
+#' @importFrom purrr map map2 reduce map_chr keep pluck map_lgl map_df
+#' @importFrom magrittr %>%
+#' @importFrom plotly filter mutate plot_ly add_markers add_lines layout
+#' ggplotly subplot add_annotations add_bars renderPlotly
+#' @importFrom shiny fluidPage titlePanel tags navlistPanel tabPanel fluidRow
+#' column h2 h3 h4 h5 fileInput selectInput numericInput radioButtons checkboxInput
+#' textInput uiOutput actionButton hr textAreaInput reactiveVal reactive
+#' observeEvent updateSelectizeInput updateTextAreaInput updateNumericInput
+#' updateSelectInput renderUI markdown plotOutput div HTML htmlOutput
+#' showModal modalDialog icon shinyApp textOutput helpText conditionalPanel
+#' @importFrom ggplot2 ggplot aes geom_line geom_point geom_polygon geom_hline
+#' scale_x_log10 scale_x_continuous scale_y_log10 scale_y_continuous xlab ylab
+#' theme ggtitle element_blank geom_segment aes_string aes_string theme_bw theme_grey
+#' @importFrom bslib accordion accordion_panel card navset_card_tab
+#' nav_panel
+#' @importFrom utils modifyList
+#' @importFrom stats as.formula na.exclude
+#' @importFrom stringr str_replace regex
+NULL
